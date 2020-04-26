089d11c0a5856f70a8ed7fa94303e243a80d4079
slsh1o <etzo_tw@ukr.net>
2020-04-26 05:45:14 +0300

    Create `geristration` view func

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import response, decorators, status, permissions
 
-# Create your views here.
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.serializers import UserRegistrationSerializer
+
+
+@decorators.api_view(['POST'])
+@decorators.permission_classes([permissions.AllowAny])
+def registration(request):
+    serializer = UserRegistrationSerializer(data=request.data)
+    if not serializer.is_valid():
+        return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    user = serializer.save()
+    refresh = RefreshToken.for_user(user)
+    res = {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+    return response.Response(res, status.HTTP_201_CREATED)
