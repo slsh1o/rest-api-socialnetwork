e50b277e7a1068486d38017fd7e6acc0c78bd269
slsh1o <etzo_tw@ukr.net>
2020-04-26 05:56:34 +0300

    Include users app urls to projects urls

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -1,7 +1,8 @@
 """URL Configuration for socialnetwork project."""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/jwtauth/', include('users.urls')),
 ]
